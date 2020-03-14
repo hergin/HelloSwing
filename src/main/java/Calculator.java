30fe79399522ec2665c1041ba432c83021f4ad28
Dot Horsman <dottyhorsman@bsu.edu>
2020-03-13 20:28:07 -0400

    Switched the system from binary to decimal, Added the following functions and their corresponding buttons: numbers 1-9, Clear (C), Multiplication, Subtraction, and Division, Default state/marker is now underscore instead of 0

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,34 +1,71 @@
 public class Calculator {
 
-    String mainNumber="0";
+    String mainNumber="_";
     String previousNumber;
 
     public Calculator() {
-        this("0");
+        this("_");
     }
 
     public Calculator(String mainNumber) {
         this.mainNumber = mainNumber;
     }
 
-    public String appendToMainNumber(String toAppend) {
-        mainNumber += toAppend;
+    public String appendToMainNumber(String toAppend)
+    {
+        if(mainNumber.equals("_"))
+        {
+            mainNumber = toAppend;
+        }
+        else
+        {
+            mainNumber += toAppend;
+        }
+
         return mainNumber;
     }
 
     public void resetMainNumber() {
-        mainNumber = "0";
+        mainNumber = "_";
     }
 
     public String getMainNumber() {
         return mainNumber;
     }
 
-    public String add(String toAdd) {
-        var mainNumberInteger = Integer.parseInt(previousNumber,2);
-        var newNumberInteger = Integer.parseInt(toAdd,2);
+    public String add(String toAdd)
+    {
+        var mainNumberInteger = Integer.parseInt(previousNumber);
+        var newNumberInteger = Integer.parseInt(toAdd);
         var result = mainNumberInteger + newNumberInteger;
-        mainNumber = Integer.toBinaryString(result);
+        mainNumber = Integer.toString(result);
+        return mainNumber;
+    }
+
+    public String subtract(String toSubtract)
+    {
+        var mainNumberInteger = Integer.parseInt(previousNumber);
+        var newNumberInteger = Integer.parseInt(toSubtract);
+        var result = mainNumberInteger - newNumberInteger;
+        mainNumber = Integer.toString(result);
+        return mainNumber;
+    }
+
+    public String multiply(String toMultiply)
+    {
+        var mainNumberInteger = Integer.parseInt(previousNumber);
+        var newNumberInteger = Integer.parseInt(toMultiply);
+        var result = mainNumberInteger * newNumberInteger;
+        mainNumber = Integer.toString(result);
+        return mainNumber;
+    }
+
+    public String divide(String toDivide)
+    {
+        var mainNumberInteger = Integer.parseInt(previousNumber);
+        var newNumberInteger = Integer.parseInt(toDivide);
+        var result = mainNumberInteger / newNumberInteger;
+        mainNumber = Integer.toString(result);
         return mainNumber;
     }
 
